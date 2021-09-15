a04478b0c27f1871c04fb355acc2a498f7516b6f
johnny austor <johnny.austor@gmail.com>
2021-09-15 20:04:22 +0700

    init get list

--- src/main/java/com/jap/rest101/controller/UserController.java
+++ src/main/java/com/jap/rest101/controller/UserController.java
@@ -4,10 +4,9 @@ import com.jap.rest101.db.entity.User;
 import com.jap.rest101.db.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -20,6 +19,12 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         repository.save(user);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUser() {
+        List<User> users = repository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
 
